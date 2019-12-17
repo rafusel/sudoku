1f819ce53ab0c0afdf822743c4bd07f09ebb0758
Lewis Able Rafuse <rafusel@mcmaster.ca>
2019-12-17 00:09:40 -0500

    Fixed check row, col, box.

--- src/solver.py
+++ src/solver.py
@@ -3,30 +3,45 @@ from random import randint
 
 # Function that takes in the puzzle as a 2D array and returns the solution.
 def solve(puzzle):
+    backtrack = []
     i, j = 0, 0
     while (i < len(puzzle)):
         while (j < len(puzzle[i])):
-            if (puzzle[i][j] == 0):
-                puzzle[i][j] = randint(1, 9)
-                if() #TODO
+            print(i, j)
+            if (puzzle[i][j] == 0): # This is an element we have to solve
+                puzzle[i][j] = 1
+                backtrack.append((i, j))
+            if (checkRow(puzzle, i) and checkCol(puzzle, j) and checkBox(puzzle, i, j)): # Does this element give us a possible solution
+                j += 1
+            else: #This element does not give a possible solution: backtrack
+                puzzle[i][j] = 0 #Reset the current element
+                backtrack.pop() #Remove this an element we need to backtrack to
+                i, j = backtrack.pop() #Get the new values we need to backtrack to
+                if (puzzle[i][j] < 9):
+                    puzzle[i][j] += 1 #Increment the value we are backtracking to
+                    #TODO need to make this conditional based on if there's a repeat in the row, col, box
+                else: 
+                    return "No solution"
+        i += 1
+        j = 0 
     return puzzle
 
-#Return boolean representing the sum of the row denoted by i is less than 45.
-def checkRow(puzzle, i):
-    return sum(puzzle[i]) =< 45
+#Remove all elements in the ith row from possible elements.
+def checkRow(possibleElems, puzzle, i):
+    for elem in puzzle[i]:
+        possibleElems.remove(elem)
 
-#Return boolean representing the sum of the col denoted by j is less than 45.
-def checkCol(puzzle, j):
-    return sum(puzzle[i][j] for i in range(len(puzzle))) =< 45
+#Remove all elements in the jth column from possible element.
+def checkCol(possibleElems, puzzle, j):
+    for i in range(9):
+        possibleElems.remove(puzzle[i][j])
 
-#Return boolean representing the sum of the box denoted by i, j is less than 45.
-def checkBox(puzzle, i, j):
+#Remove all elements
+def checkBox(possibleElems, puzzle, i, j):
     quadrant = ((i//3) + 1, (j//3) + 1) #Which quadrant is the current place in.
-    sum = 0
     for a in range(1, 4):
         for b in range(1, 4):
-            sum += puzzle[a * quadrant[0] - 1][b * quadrant[1] - 1] #Indexing trickery to get the indices in square.
-    return sum =< 45
+            possibleElems.remove(puzzle[a * quadrant[0] - 1][b * quadrant[1] - 1]) #Indexing trickery to get the indices in square.
 
 #Parse a string version of the given input puzzle.
 def parsePuzzle(stringPuzzle):
@@ -48,6 +63,6 @@ puzzle = '''
 '''
 
 print(parsePuzzle(puzzle))
-print(checkBox(parsePuzzle(puzzle), 3, 3))
+#print(checkBox(parsePuzzle(puzzle), 3, 3))
 
-#print(solve(parsePuzzle(puzzle)))
+print(solve(parsePuzzle(puzzle)))
