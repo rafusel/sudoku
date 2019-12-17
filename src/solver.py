0145b3f48e3ff8e3ce8abcd53e980c89ee5b3216
Lewis Able Rafuse <rafusel@mcmaster.ca>
2019-12-17 01:01:35 -0500

    Debugging with print statements.

--- src/solver.py
+++ src/solver.py
@@ -8,40 +8,54 @@ def solve(puzzle):
     while (i < len(puzzle)):
         while (j < len(puzzle[i])):
             print(i, j)
-            if (puzzle[i][j] == 0): # This is an element we have to solve
-                puzzle[i][j] = 1
+            if (puzzle[i][j] == 0): # This is an element we may have to backtrack to.
+                print("This is a 0 element")
                 backtrack.append((i, j))
-            if (checkRow(puzzle, i) and checkCol(puzzle, j) and checkBox(puzzle, i, j)): # Does this element give us a possible solution
+
+
+            if (len(backtrack) > 0 and backtrack[-1] == (i, j)): #We have to try and solve this element currently
+                print("Solving this element")
+                possibleElems = set()
+                for e in range(puzzle[i][j] + 1, 10): #Get a set of all the possible elements that we could solve with.
+                    possibleElems.add(e)
+                checkRow(possibleElems, puzzle, i)
+                checkCol(possibleElems, puzzle, j)
+                checkBox(possibleElems, puzzle, i, j)
+                if (len(possibleElems) > 0): # Does this element give us a possible solution
+                    print("This element's solution is: " + str(min(possibleElems)))
+                    puzzle[i][j] = min(possibleElems)
+                    j += 1
+                else: #This element does not give a possible solution: backtrack
+                    print("No solution, backtracking")
+                    puzzle[i][j] = 0 #Reset the current element
+                    backtrack.pop() #Remove this an element we need to backtrack to
+                    i, j = backtrack.pop() #Get the new values we need to backtrack to
+                    print("Backtracking to: ", str(i), str(j))
+            else: #Don't need to solve this one.
+                print("Not solving this one")
                 j += 1
-            else: #This element does not give a possible solution: backtrack
-                puzzle[i][j] = 0 #Reset the current element
-                backtrack.pop() #Remove this an element we need to backtrack to
-                i, j = backtrack.pop() #Get the new values we need to backtrack to
-                if (puzzle[i][j] < 9):
-                    puzzle[i][j] += 1 #Increment the value we are backtracking to
-                    #TODO need to make this conditional based on if there's a repeat in the row, col, box
-                else: 
-                    return "No solution"
+            printPuzzle(puzzle)
+            print()
         i += 1
         j = 0 
     return puzzle
 
 #Remove all elements in the ith row from possible elements.
 def checkRow(possibleElems, puzzle, i):
-    for elem in puzzle[i]:
-        possibleElems.remove(elem)
+    for j in range(len(puzzle[i])):
+        possibleElems.discard(puzzle[i][j])
 
 #Remove all elements in the jth column from possible element.
 def checkCol(possibleElems, puzzle, j):
     for i in range(9):
-        possibleElems.remove(puzzle[i][j])
+        possibleElems.discard(puzzle[i][j])
 
 #Remove all elements
 def checkBox(possibleElems, puzzle, i, j):
     quadrant = ((i//3) + 1, (j//3) + 1) #Which quadrant is the current place in.
     for a in range(1, 4):
         for b in range(1, 4):
-            possibleElems.remove(puzzle[a * quadrant[0] - 1][b * quadrant[1] - 1]) #Indexing trickery to get the indices in square.
+            possibleElems.discard(puzzle[a * quadrant[0] - 1][b * quadrant[1] - 1]) #Indexing trickery to get the indices in square.
 
 #Parse a string version of the given input puzzle.
 def parsePuzzle(stringPuzzle):
@@ -61,8 +75,14 @@ puzzle = '''
 0 0 0 0 0 0 0 7 4
 0 0 5 2 0 6 3 0 0
 '''
+def printPuzzle(puzzle):
+    for i in range(9):
+        for j in range(9):
+            print(puzzle[i][j], end="")
+            print(" ", end="")
+        print()
 
-print(parsePuzzle(puzzle))
+#print(parsePuzzle(puzzle))
 #print(checkBox(parsePuzzle(puzzle), 3, 3))
 
 print(solve(parsePuzzle(puzzle)))
