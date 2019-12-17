4fb13d1b09ac878e123ac421f743e16c982f77f6
Lewis Able Rafuse <rafusel@mcmaster.ca>
2019-12-16 21:14:57 -0500

    Added the row, col and box checkers.

--- src/solver.py
+++ src/solver.py
@@ -3,12 +3,30 @@ from random import randint
 
 # Function that takes in the puzzle as a 2D array and returns the solution.
 def solve(puzzle):
-    #stuff
-    for i in range(len(puzzle)):
-        for j in range(len(puzzle[i])):
+    i, j = 0, 0
+    while (i < len(puzzle)):
+        while (j < len(puzzle[i])):
             if (puzzle[i][j] == 0):
                 puzzle[i][j] = randint(1, 9)
+                if() #TODO
     return puzzle
+
+#Return boolean representing the sum of the row denoted by i is less than 45.
+def checkRow(puzzle, i):
+    return sum(puzzle[i]) =< 45
+
+#Return boolean representing the sum of the col denoted by j is less than 45.
+def checkCol(puzzle, j):
+    return sum(puzzle[i][j] for i in range(len(puzzle))) =< 45
+
+#Return boolean representing the sum of the box denoted by i, j is less than 45.
+def checkBox(puzzle, i, j):
+    quadrant = ((i//3) + 1, (j//3) + 1) #Which quadrant is the current place in.
+    sum = 0
+    for a in range(1, 4):
+        for b in range(1, 4):
+            sum += puzzle[a * quadrant[0] - 1][b * quadrant[1] - 1] #Indexing trickery to get the indices in square.
+    return sum =< 45
 
 #Parse a string version of the given input puzzle.
 def parsePuzzle(stringPuzzle):
@@ -17,8 +35,6 @@ def parsePuzzle(stringPuzzle):
     puzzle = [puzzle[i].split(" ") for i in range(len(puzzle))]
     return [list(map(lambda a: int(a), puzzle[i])) for i in range(len(puzzle))]
             
-
-
 puzzle = '''
 3 0 6 5 0 8 4 0 0
 5 2 0 0 0 0 0 0 0
@@ -31,4 +47,7 @@ puzzle = '''
 0 0 5 2 0 6 3 0 0
 '''
 
-print(solve(parsePuzzle(puzzle)))
+print(parsePuzzle(puzzle))
+print(checkBox(parsePuzzle(puzzle), 3, 3))
+
+#print(solve(parsePuzzle(puzzle)))
